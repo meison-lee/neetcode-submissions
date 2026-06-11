e84535f9ceb84af0eb9fc7f65bec10a53ab739ce
Meison Lee <84371764+meison-lee@users.noreply.github.com>
2026-06-11 18:53:40 +0800

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # because the array is in ascending order, so we can use binary seacrh
+                
+        left = 0 
+        right = len(nums) - 1
+        
+        while (right - left) > 1:
+            middle = (left + right)//2            
+            if nums[middle] == target: 
+                return middle
+            elif nums[middle] > target: 
+                right = middle
+            else : 
+                left = middle   
+        
+        if nums[left] == target:
+            return left
+        elif nums[right] == target:
+            return right
+        else:
+            return -1
